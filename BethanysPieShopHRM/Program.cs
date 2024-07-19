9a3e04347de9a2922959a74406feccd0135bda79
Dhon <morgangrimes14@gmail.com>
2024-07-19 09:10:28 -0500

    datetime

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -62,4 +62,24 @@ bool isDigit = char.IsDigit(userSelection);
 
 bool isLetter = char.IsLetter(userSelection);
 
+DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+Console.WriteLine(hireDate);
+
+DateTime exitDate = new DateTime(2025, 12, 11);
+
+// DateTime invalidDate = new DateTime(2025, 15, 11);
+
+DateTime startDate = hireDate.AddDays(15);
+Console.WriteLine(startDate);
+
+DateTime currentDate = DateTime.Now;
+bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+DateTime startHour = DateTime.Now;
+TimeSpan workTime = new TimeSpan(8, 35, 0);
+DateTime endHour = startHour.Add(workTime);
+
+Console.WriteLine(startHour.ToLongDateString());
+Console.WriteLine(endHour.ToShortTimeString());
+
 Console.ReadLine();
