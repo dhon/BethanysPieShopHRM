d14230e73b871fa9b8975cc4930b7d9848007c0b
Dhon <morgangrimes14@gmail.com>
2024-07-19 09:32:41 -0500

    relational operators

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,95 +1,11 @@
-﻿
+﻿int age = 23;
 
-//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+bool a = age == 23;
 
-//Console.WriteLine("Please enter your name: ");
+Console.WriteLine("Age is 23: " + a);
 
-//string name = Console.ReadLine();
+bool b  = age >= 23;
+Console.WriteLine("Age is greater than 12: " + b);
 
-//int monthlyWage = 1234;
-
-//int months = 12, bonus = 1000;
-
-//bool isActive = true;
-
-//double rating = 99.25;
-
-// byte numberOfEmployees = 300;
-
-//int hoursWorked = 125;
-
-//hoursWorked = 148;
-
-// monthlyWage = true;
-
-//const double interestRate = 0.07;
-
-// interestRate = 0.08;
-
-//string firstName = "Bethany";
-//string lastName = "Smith";
-
-//string emptyString = "";
-
-// Console.WriteLine("Please enter your name");
-// string s = Console.ReadLine();
-
-//double ratePerHour = 12.34;
-//int numberOfHoursWorked = 165;
-
-//double currentMonthWage = ratePerHour * numberOfHoursWorked;
-//Console.WriteLine(currentMonthWage);
-
-//ratePerHour += 3;
-//Console.WriteLine(ratePerHour);
-
-//if (currentMonthWage > 2000)
-//    Console.WriteLine("Top paid employee!");
-
-//int numberOfEmployees = 15;
-//numberOfEmployees--;
-
-//bool a;
-//int b;
-
-//int intMaxValue = int.MaxValue;
-//int intMinValue = int.MinValue;
-
-//char userSelection = 'a';
-//char upperVersion = char.ToUpper(userSelection);
-
-//bool isDigit = char.IsDigit(userSelection);
-
-//bool isLetter = char.IsLetter(userSelection);
-
-//DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-//Console.WriteLine(hireDate);
-
-//DateTime exitDate = new DateTime(2025, 12, 11);
-
-// DateTime invalidDate = new DateTime(2025, 15, 11);
-
-//DateTime startDate = hireDate.AddDays(15);
-//Console.WriteLine(startDate);
-
-//DateTime currentDate = DateTime.Now;
-//bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-//DateTime startHour = DateTime.Now;
-//TimeSpan workTime = new TimeSpan(8, 35, 0);
-//DateTime endHour = startHour.Add(workTime);
-
-//Console.WriteLine(startHour.ToLongDateString());
-//Console.WriteLine(endHour.ToShortTimeString());
-
-//int numberOfHoursWorked = 165;
-
-//long veryLongMonth = numberOfHoursWorked;
-
-//double d = 123456789.0;
-
-//int x = (int)d;
-
-//int intVeryLongMonth = (int)veryLongMonth;
-
-Console.ReadLine();
+bool c = (age >= 18) && (age <= 65);
+Console.WriteLine("Age is between 18 and 65" + c);
