7ec179baf02860f0b1894727ce13c27e4529caa6
Dhon <morgangrimes14@gmail.com>
2024-07-19 08:49:12 -0500

    primitive types

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -5,3 +5,19 @@ Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 Console.WriteLine("Please enter your name: ");
 
 string name = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+// byte numberOfEmployees = 300;
+
+int hoursWorked = 125;
+
+hoursWorked = 148;
+
+// monthlyWage = true;
