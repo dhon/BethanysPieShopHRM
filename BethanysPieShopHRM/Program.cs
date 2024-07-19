d007c09d9d6191d5d700b1b05c79f2ed251d3d0b
Dhon <morgangrimes14@gmail.com>
2024-07-19 09:20:46 -0500

    converting between types

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,85 +1,95 @@
 ﻿
 
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-Console.WriteLine("Please enter your name: ");
+//Console.WriteLine("Please enter your name: ");
 
-string name = Console.ReadLine();
+//string name = Console.ReadLine();
 
-int monthlyWage = 1234;
+//int monthlyWage = 1234;
 
-int months = 12, bonus = 1000;
+//int months = 12, bonus = 1000;
 
-bool isActive = true;
+//bool isActive = true;
 
-double rating = 99.25;
+//double rating = 99.25;
 
 // byte numberOfEmployees = 300;
 
-int hoursWorked = 125;
+//int hoursWorked = 125;
 
-hoursWorked = 148;
+//hoursWorked = 148;
 
 // monthlyWage = true;
 
-const double interestRate = 0.07;
+//const double interestRate = 0.07;
 
 // interestRate = 0.08;
 
-string firstName = "Bethany";
-string lastName = "Smith";
+//string firstName = "Bethany";
+//string lastName = "Smith";
 
-string emptyString = "";
+//string emptyString = "";
 
 // Console.WriteLine("Please enter your name");
 // string s = Console.ReadLine();
 
-double ratePerHour = 12.34;
-int numberOfHoursWorked = 165;
+//double ratePerHour = 12.34;
+//int numberOfHoursWorked = 165;
 
-double currentMonthWage = ratePerHour * numberOfHoursWorked;
-Console.WriteLine(currentMonthWage);
+//double currentMonthWage = ratePerHour * numberOfHoursWorked;
+//Console.WriteLine(currentMonthWage);
 
-ratePerHour += 3;
-Console.WriteLine(ratePerHour);
+//ratePerHour += 3;
+//Console.WriteLine(ratePerHour);
 
-if (currentMonthWage > 2000)
-    Console.WriteLine("Top paid employee!");
+//if (currentMonthWage > 2000)
+//    Console.WriteLine("Top paid employee!");
 
-int numberOfEmployees = 15;
-numberOfEmployees--;
+//int numberOfEmployees = 15;
+//numberOfEmployees--;
 
-bool a;
-int b;
+//bool a;
+//int b;
 
-int intMaxValue = int.MaxValue;
-int intMinValue = int.MinValue;
+//int intMaxValue = int.MaxValue;
+//int intMinValue = int.MinValue;
 
-char userSelection = 'a';
-char upperVersion = char.ToUpper(userSelection);
+//char userSelection = 'a';
+//char upperVersion = char.ToUpper(userSelection);
 
-bool isDigit = char.IsDigit(userSelection);
+//bool isDigit = char.IsDigit(userSelection);
 
-bool isLetter = char.IsLetter(userSelection);
+//bool isLetter = char.IsLetter(userSelection);
 
-DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-Console.WriteLine(hireDate);
+//DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+//Console.WriteLine(hireDate);
 
-DateTime exitDate = new DateTime(2025, 12, 11);
+//DateTime exitDate = new DateTime(2025, 12, 11);
 
 // DateTime invalidDate = new DateTime(2025, 15, 11);
 
-DateTime startDate = hireDate.AddDays(15);
-Console.WriteLine(startDate);
+//DateTime startDate = hireDate.AddDays(15);
+//Console.WriteLine(startDate);
 
-DateTime currentDate = DateTime.Now;
-bool areWeInDst = currentDate.IsDaylightSavingTime();
+//DateTime currentDate = DateTime.Now;
+//bool areWeInDst = currentDate.IsDaylightSavingTime();
 
-DateTime startHour = DateTime.Now;
-TimeSpan workTime = new TimeSpan(8, 35, 0);
-DateTime endHour = startHour.Add(workTime);
+//DateTime startHour = DateTime.Now;
+//TimeSpan workTime = new TimeSpan(8, 35, 0);
+//DateTime endHour = startHour.Add(workTime);
 
-Console.WriteLine(startHour.ToLongDateString());
-Console.WriteLine(endHour.ToShortTimeString());
+//Console.WriteLine(startHour.ToLongDateString());
+//Console.WriteLine(endHour.ToShortTimeString());
+
+int numberOfHoursWorked = 165;
+
+long veryLongMonth = numberOfHoursWorked;
+
+double d = 123456789.0;
+
+int x = (int)d;
+
+int intVeryLongMonth = (int)veryLongMonth;
 
 Console.ReadLine();
